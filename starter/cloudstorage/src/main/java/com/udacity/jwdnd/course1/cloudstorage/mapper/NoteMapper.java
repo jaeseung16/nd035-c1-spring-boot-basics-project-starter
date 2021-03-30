2607083deff43dd25277d8620c839931c173d8cf
Jae-Seung Lee <jaeseung@gmail.com>
2021-03-30 19:00:38 -0400

    feat: NoteService

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
@@ -0,0 +1,26 @@
+package com.udacity.jwdnd.course1.cloudstorage.mapper;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface NoteMapper {
+    @Select("SELECT * FROM NOTES WHERE userid = #{userid}")
+    List<Note> getNotes(Integer userid);
+
+    @Select("SELECT * FROM NOTES WHERE noteid = #{noteid}")
+    Note getNote(Integer noteid);
+
+    @Insert("INSERT INTO NOTES (notetitle, notedescription, userid) " +
+            "VALUES(#{notetitle}, #{notedescription}, #{userid})")
+    @Options(useGeneratedKeys = true, keyProperty = "noteid")
+    Integer insert(Note note);
+
+    @Update("UPDATE NOTES SET notetitle=#{notetitle}, notedescription=#{notedescription} WHERE noteid = #{noteid}")
+    void update(Note note);
+
+    @Delete("DELETE FROM NOTES WHERE noteid = #{noteid}")
+    void delete(Integer noteid);
+}
